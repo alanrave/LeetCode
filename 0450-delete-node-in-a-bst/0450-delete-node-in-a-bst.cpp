d48af542808f1f56a6d8f46c2f86adc2103c9404
alanrave <152051923+alanrave@users.noreply.github.com>
2025-03-06 22:50:16 +0530

    Time: 0 ms (100%), Space: 34.2 MB (63.56%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -11,20 +11,48 @@
  */
 class Solution {
 public:
+     TreeNode *getSuccessor(TreeNode * curr, int key)
+        {
+           curr=curr->right;
+           while(curr!=NULL && curr->left!=NULL)
+           {
+            curr=curr->left;
+           }
+           return curr;
+        }
     TreeNode* deleteNode(TreeNode* root, int key) {
-         if(root) 
-            if(key < root->val) root->left = deleteNode(root->left, key);     //We frecursively call the function until we find the target node
-            else if(key > root->val) root->right = deleteNode(root->right, key);       
-            else{
-                if(!root->left && !root->right) return NULL;          //No child condition
-                if (!root->left || !root->right)
-                    return root->left ? root->left : root->right;    //One child contion -> replace the node with it's child
-					                                                //Two child condition   
-                TreeNode* temp = root->left;                        //(or) TreeNode *temp = root->right;
-                while(temp->right != NULL) temp = temp->right;     //      while(temp->left != NULL) temp = temp->left;
-                root->val = temp->val;                            //       root->val = temp->val;
-                root->left = deleteNode(root->left, temp->val);  //        root->right = deleteNode(root->right, temp);		
-            }
+        if(root==NULL)
         return root;
+        if(root->val<key)
+        {
+           root->right= deleteNode(root->right,key);
+        }
+        else if(root->val>key)
+        {
+            root->left=deleteNode(root->left,key);
+        }
+        else
+        {
+            if(root->left==NULL)
+            {
+                TreeNode *temp=root->right;
+                delete root;
+                return temp;
+            }
+            else if(root->right==NULL)
+            {
+                TreeNode *temp=root->left;
+                delete root;
+                return temp;
+            }
+            else
+            {
+                TreeNode *succ=getSuccessor(root,key);
+                root->val=succ->val;
+                root->right= deleteNode(root->right,succ->val);
+            }
+            
+        }
+       return root;
     }
 };
