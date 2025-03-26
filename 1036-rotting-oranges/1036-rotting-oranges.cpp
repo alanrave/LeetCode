7af5bb20f2ea25c95ad0a5d51e0fa77171f4d2a1
alanrave <152051923+alanrave@users.noreply.github.com>
2025-03-26 18:59:21 +0530

    Time: 0 ms (100%), Space: 17.2 MB (27.51%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        queue<pair<pair<int,int>,int>>q;
         int n=grid.size();
-        //rows
         int m=grid[0].size();
-        //columns;
         vector<vector<int>>vis(n,vector<int>(m,0));
+        queue<pair<pair<int,int>,int>>q;
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
@@ -20,30 +18,32 @@ public:
         }
         int tm=0;
         int drow[]={-1,0,1,0};
-        int dcol[]={0,1,0,-1};
+        int dcol[]={0,-1,0,1};
         while(!q.empty())
         {
+            
             int r=q.front().first.first;
             int c=q.front().first.second;
             int t=q.front().second;
+            tm=max(tm,t);
             q.pop();
-            tm=max(t,tm);
             for(int i=0;i<4;i++)
             {
-              int nrow=drow[i]+r;
-              int ncol=dcol[i]+c;
-              if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]!=2 && grid[nrow][ncol]==1)
-              {
+               int nrow=drow[i]+r;
+               int ncol=dcol[i]+c;
+               if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && 
+               grid[nrow][ncol]==1 && vis[nrow][ncol]!=2)
+               {
                 q.push({{nrow,ncol},t+1});
                 vis[nrow][ncol]=2;
-              }   
+               }
             }
         }
         for(int i=0;i<n;i++)
         {
             for(int j=0;j<m;j++)
             {
-                if(vis[i][j]!=2 && grid[i][j]==1)
+                if(grid[i][j]==1 && vis[i][j]!=2)
                 return -1;
             }
         }
